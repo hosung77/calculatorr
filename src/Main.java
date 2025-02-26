9dcb970c659b1f2dfe6c4c12c05d1d5108bf39fc
hosung77 <ho4728@gmail.com>
2025-02-26 14:33:57 +0900

    calculator_necc_1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+        System.out.print("숫자를 입력해주세요: ");
+        int num1 = scanner.nextInt();
+
+        if (num1 >= 1) {
+            System.out.println("입력된 숫자: " + num1);
+        } else {
+            System.out.println("0보다 큰 숫자를 입력해주세요.");
         }
+
+        System.out.print("기호를 입력하세요: ");
+        char sym = scanner.next().charAt(0);
+
+        System.out.println("숫자를 입력해주세요: ");
+        int num2 = scanner.nextInt();
+
+        if (num2 >= 1) {
+            System.out.println("입력된 숫자: " + num2);
+        } else {
+            System.out.println("0보다 큰 숫자를 입력해주세요.");
+        }
+
     }
 }
